c7460ba3dc3df41cb795b4fdcdc74194b0488e0b
FS48727 <fsalomon@uni-muenster.de>
2024-11-06 14:42:20 +0800

    update

---
+++ 8_bmi.py
@@ -0,0 +1,15 @@
+#
+# Frederik
+# BMI Calculator
+#
+ 
+while True:
+    w = float(input('Enter uour weight in kg      : '))
+    h = float(input('Enter uour height in meters  : '))
+
+    bmi = w/h**2
+
+    print(f'Your BMI is: {bmi}')
+    answer = input('Continue? (yes/no) ')
+    if answer == "no":
+        break
